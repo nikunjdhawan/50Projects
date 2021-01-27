4ead8417a4a1b6fd827cfbbf287150c6331bf035
Nikunj Dhawan <nikunjdhawan@gmail.com>
2021-01-27 18:23:12 +0530

    ok

--- HTML 5 CODING YEAHHHHH/script.js
+++ HTML 5 CODING YEAHHHHH/script.js
@@ -20,3 +20,6 @@ const container = document.querySelector('.container')
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+
+
